dcbe8107832060495f1b8fcb351a43c9f001f3a1
jovan-stojkovic <code.jovanstojkovic@gmail.com>
2024-03-22 09:59:00 +0100

    navbar style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,26 @@
 const Navbar = () => {
-  return <>
-  <div className="navbar">
-  <div className="navbar-left-side">
-  <a href="/">
-    <img src="./src/assets/images/logo.svg" alt="logo" />
-  </a>
-   <div className="navs">
-    <p className="nav">Collections</p>
-    <p className="nav">Men</p>
-    <p className="nav">Women</p>
-    <p className="nav">About</p>
-    <p className="nav">Contact</p>
-   </div>
-  </div>
-  <div className="right-side">
-    <button className="cart">
-      <img src="./src/assets/images/icon-cart.svg" alt="cart" />
-    </button>
-    <div className="user">
-      <img src="./src/assets/images/image-avatar.png" alt="user-img" />
+  return (
+    <div className="navbar">
+      <div className="navbar-left-side">
+        <a href="/">
+          <img src="./src/assets/images/logo.svg" alt="logo" />
+        </a>
+        <div className="navs">
+          <p className="nav">Collections</p>
+          <p className="nav">Men</p>
+          <p className="nav">Women</p>
+          <p className="nav">About</p>
+          <p className="nav">Contact</p>
+        </div>
+      </div>
+      <div className="navbar-right-side">
+        <button className="cart">
+        </button>
+        <div className="user">
+        </div>
+      </div>
     </div>
-  </div>
-  </div>
-  </>;
+  );
 };
 
 export default Navbar;
